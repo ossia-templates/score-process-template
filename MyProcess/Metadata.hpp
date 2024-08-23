8a67c19aef3d9b00b46d2ee097ab5d6070110065
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2024-08-23 12:47:09 -0400

    [ci] Update template

--- MyProcess/Metadata.hpp
+++ MyProcess/Metadata.hpp
@@ -6,18 +6,18 @@ namespace MyProcess
 class Model;
 }
 
-PROCESS_METADATA(
-    ,
-    MyProcess::Model,
-    "00000000-0000-0000-0000-000000000000",
-    "MyProcess",                                  // Internal name
-    "MyProcess",                                  // Pretty name
-    Process::ProcessCategory::Other,              // Category
-    "Other",                                      // Category
-    "Description",                                // Description
-    "Author",                                     // Author
-    (QStringList{"Put", "Your", "Tags", "Here"}), // Tags
-    {},                                           // Inputs
-    {},                                           // Outputs
-    Process::ProcessFlags::SupportsAll            // Flags
+PROCESS_METADATA(,
+                 MyProcess::Model,
+                 "00000000-0000-0000-0000-000000000000",
+                 "MyProcess",                                  // Internal name
+                 "MyProcess",                                  // Pretty name
+                 Process::ProcessCategory::Other,              // Category
+                 "Other",                                      // Category
+                 "Description",                                // Description
+                 "Author",                                     // Author
+                 (QStringList{"Put", "Your", "Tags", "Here"}), // Tags
+                 {},                                           // Inputs
+                 {},                                           // Outputs
+                 QUrl{},                                       // Doc url
+                 Process::ProcessFlags::SupportsAll            // Flags
 )
